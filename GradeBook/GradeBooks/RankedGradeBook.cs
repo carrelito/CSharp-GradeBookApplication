662d5f2805833264bc15ea190fd24ffa844a20e9
Gatien Fangwa Tikanwou <GTikanwou@slb.com>
2019-02-08 16:31:35 +0100

    GetLetterGrade

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,6 +1,7 @@
 ﻿using GradeBook.Enums;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace GradeBook.GradeBooks
@@ -10,6 +11,53 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             this.Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if(Students.Count< 5)
+            {
+                throw new InvalidOperationException("Ranked - grading requires a minimum of 5 students to work");
+            }
+            if (GetGradeA(averageGrade))
+                return 'A';
+            else if (GetGradeB(averageGrade))
+                return 'B';
+            else if (GetGradeC(averageGrade))
+                return 'C';
+            else if (GetGradeD(averageGrade))
+                return 'D';
+            else
+            return 'F';
+        }
+
+        private bool GetGradeA( double averageGrade)
+        {
+            return GetGrade(averageGrade, 20);
+        }
+
+        private bool GetGradeB(double averageGrade)
+        {
+            return GetGrade(averageGrade, 40);
+        }
+        private bool GetGradeC(double averageGrade)
+        {
+            return GetGrade(averageGrade, 60);
+        }
+        private bool GetGradeD(double averageGrade)
+        {
+            return GetGrade(averageGrade, 80);
+        }
+
+        private bool GetGrade(double averageGrade, int pourcentage)
+        {
+            int index = 0;
+            index = (int)Math.Ceiling((double)(pourcentage * Students.Count / 100));
+            var grades = Students.OrderByDescending(x => x.AverageGrade).Select(x => x.AverageGrade).ToList();
+            if (averageGrade >= grades[index-1])
+                return true;
+            else
+                return false;
         }
 
     }
